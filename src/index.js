8baef7be6be088fc72772334fd0def4e951d0ffb
Jonny Howle <jonny.howle@gmail.com>
2022-07-06 19:53:56 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ let form = document.querySelector('#create-task-form');
+ form.addEventListener('submit', buildTask)
 });
+
+function buildTask(task) {
+  task.preventDefault()
+  let input = task.target['new-task-description'].value
+  appendTask(input)
+  task.target['new-task-description'].value = ""
+}
+
+function appendTask(input){
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+
+  li.textContent = `${input} `
+  li.appendChild(btn)
+
+  ul.appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
